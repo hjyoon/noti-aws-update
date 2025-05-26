8775d72e0f976edd6e97a9e2ab6f5d7cebf30501
hjyoon <hjyoon@kakao.com>
2025-05-26 17:31:00 +0900

    feat: Add run script and drop existing tables in init script

---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+echo "실행 방식 선택:"
+echo "1) Go build/run"
+echo "2) Docker Compose Up"
+echo "3) Go build/run + init DB"
+echo "4) Docker Compose Up + init DB"
+printf "번호 입력: "
+read -r opt
+
+case "$opt" in
+  1)
+    go mod tidy
+    go build -o ./build/myapp
+    ./build/myapp
+    ;;
+  2)
+    docker-compose down
+    docker-compose --env-file ./.env up --build
+    ;;
+  3)
+    . ./.env
+    PGPASSWORD="$DATABASE_PASSWORD" psql -h "$DATABASE_HOST" -U "$DATABASE_USER" -d "$DATABASE_DB" -f ./initdb/init.sql
+    go mod tidy
+    go build -o ./build/myapp
+    ./build/myapp
+    ;;
+  4)
+    docker-compose down -v
+    docker-compose --env-file ./.env up --build
+    ;;
+  *)
+    echo "올바른 번호를 선택하세요."
+    ;;
+esac
