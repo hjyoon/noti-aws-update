dad073cd46f997724dad6b5ad3b085a94b3e67ac
hjyoon <hjyoon@kakao.com>
2025-05-26 13:09:59 +0900

    feat: Add logging middleware for HTTP server requests

--- http.go
+++ http.go
@@ -10,12 +10,14 @@ import (
 )
 
 func StartHTTPServer(conn *pgx.Conn, port string) {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		ctx, cancel := context.WithTimeout(r.Context(), 2*time.Second)
 		defer cancel()
 		if err := conn.Ping(ctx); err != nil {
@@ -28,7 +30,44 @@ func StartHTTPServer(conn *pgx.Conn, port string) {
 
 	addr := ":" + port
 	log.Printf("Start Server: http://localhost%s", addr)
-	if err := http.ListenAndServe(addr, nil); err != nil {
+	if err := http.ListenAndServe(addr, LoggingMiddleware(mux)); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// CustomResponseWriter는 응답 상태 코드를 기록
+type CustomResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (w *CustomResponseWriter) WriteHeader(code int) {
+	w.statusCode = code
+	w.ResponseWriter.WriteHeader(code)
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		crw := &CustomResponseWriter{w, http.StatusOK}
+
+		next.ServeHTTP(crw, r)
+
+		duration := time.Since(start)
+		query := ""
+		if r.URL.RawQuery != "" {
+			query = "?" + r.URL.RawQuery
+		}
+		log.Printf(
+			"[%s] \"%s %s%s\" %d \"%s\" (duration: %s)",
+			r.RemoteAddr,
+			r.Method,
+			r.URL.Path,
+			query,
+			crw.statusCode,
+			r.UserAgent(),
+			duration,
+		)
+
+	})
 }
